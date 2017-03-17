d3c90073eb50725b872312a37c3a61fe5219ae76
ddh63 <ddh63@zips.uakron.edu>
2017-03-17 18:03:42 -0400

    added most of the ability to register, also added validator package to check for valid email when registering

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
 module.exports = function(app, pool) {
 	var mysql = require('mysql');
+	var validator = require('validator');
 	var bcrypt = require('bcrypt');
 	const saltRounds = 10;
 
@@ -18,7 +19,7 @@ module.exports = function(app, pool) {
 		pool.getConnection(function(err, conn) {
 			if (err) throw err;
 
-			var query = 'select * from users where username='+conn.escape(user)+' LIMIT 1';
+			var query = 'select * from users where username='+conn.escape(user)+' limit 1';
 
 			conn.query(query, function(err, result) {
 				if (err) throw err;
@@ -39,23 +40,45 @@ module.exports = function(app, pool) {
 
 	});
 
+	// TODO: check if email is in use
+	// 			 minimum(8) and maximum(72) password length
 	app.post('/api/register', function(req, res) {
 		var user = req.body.username;
 		var email = req.body.email;
 		var pass = req.body.password;
 		var conpass = req.body.confirmpassword;
 
-		if (pass == conpass) {
-			console.log("we good");
-		
-			var hashedPass = '';
-			bcrypt.hash(pass, saltRounds, function(err, hash) {
-				hashedPass = hash;
-			});
+		if (validator.isEmail(email) && (pass == conpass)) {
+			var available = false;
+			pool.getConnection(function(err, conn) {
+				if (err) throw err;
 
-			res.redirect('/');
+				var query = 'select * from users where username='+conn.escape(user)+' limit 1';
+				conn.query(query, function(err, result) {
+					if (err) throw err;
+					if (!result.length) {
+						bcrypt.hash(pass, saltRounds, function(err, hash) {
+							var insertquery = 'insert into users(username, email, password) values ('+conn.escape(user)+','+conn.escape(email)+','+conn.escape(hash)+')';
+							conn.query(insertquery, function(err, insresult) {
+								if (err) throw err;
+								sess = req.session;
+								sess.username = user;
+								res.redirect('/');
+							});
+						});
+					}
+					else {
+						console.log('username already taken');
+						res.redirect('../register');
+					}
+				});
+				conn.release();
+			});
 		}
-		
+		else {
+			console.log('Invalid email or passwords do not match.');
+			res.redirect('../register');
+		}		
 	});
 
 	app.get('/api/logout', function(req, res) {
