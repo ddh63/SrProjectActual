822ad5e0b4065c09f48ee90d82eec1c82e81a657
ddh63 <ddh63@zips.uakron.edu>
2017-03-18 17:54:42 -0400

    added client side logic for login and register that sends ajax request to server. This allows for messages to be shown to the user if needed. Also added min and max length password and email already in use check

--- api.js
+++ api.js
@@ -13,8 +13,8 @@ module.exports = function(app, pool) {
 	});
 
 	app.post('/api/login', function(req, res) {
-		var user = req.body.username;
-		var pass = req.body.password;
+		var user = req.body.user;
+		var pass = req.body.pass;
 
 		pool.getConnection(function(err, conn) {
 			if (err) throw err;
@@ -29,63 +29,70 @@ module.exports = function(app, pool) {
 						if (passresult) {
 							sess = req.session;
 							sess.username = user;
-							res.redirect('/');
+							res.send('');
 						}
 						else {
-							res.redirect('../login');
+							res.send('<h4>Incorrect password</h4>');
 						}
 					});
 				}
 				else {
-					res.redirect('../login');
+					res.send('<h4>Invalid username</h4>');
 				}
 
 			});
 
 			conn.release();
 		});
-
 	});
 
-	// TODO: check if email is in use
-	// 			 minimum(8) and maximum(72) password length
 	app.post('/api/register', function(req, res) {
-		var user = req.body.username;
+		var user = req.body.user;
 		var email = req.body.email;
-		var pass = req.body.password;
-		var conpass = req.body.confirmpassword;
+		var pass = req.body.pass;
+		var conpass = req.body.confirmpass;
 
-		if (validator.isEmail(email) && (pass == conpass)) {
-			var available = false;
-			pool.getConnection(function(err, conn) {
-				if (err) throw err;
-
-				var query = 'select * from users where username='+conn.escape(user)+' limit 1';
-				conn.query(query, function(err, result) {
-					if (err) throw err;
-					if (!result.length) {
-						bcrypt.hash(pass, saltRounds, function(err, hash) {
-							var insertquery = 'insert into users(username, email, password) values ('+conn.escape(user)+','+conn.escape(email)+','+conn.escape(hash)+')';
-							conn.query(insertquery, function(err, insresult) {
-								if (err) throw err;
-								sess = req.session;
-								sess.username = user;
-								res.redirect('/');
-							});
-						});
-					}
-					else {
-						console.log('username already taken');
-						res.redirect('../register');
-					}
-				});
-				conn.release();
-			});
+		if (pass.length < 6 || pass.length > 72) {
+			res.send('<h4>Password must be 6 to 72 characters long</h4>');
 		}
 		else {
-			console.log('Invalid email or passwords do not match.');
-			res.redirect('../register');
-		}		
+		var validEmail = validator.isEmail(email);
+
+			if (validEmail && (pass == conpass)) {
+				var available = false;
+				pool.getConnection(function(err, conn) {
+					if (err) throw err;
+
+					var query = 'select * from users where username='+conn.escape(user)+' or email='+conn.escape(email)+' limit 1';
+					conn.query(query, function(err, result) {
+						if (err) throw err;
+						if (!result.length) {
+							bcrypt.hash(pass, saltRounds, function(err, hash) {
+								var insertquery = 'insert into users(username, email, password) values ('+conn.escape(user)+','+conn.escape(email)+','+conn.escape(hash)+')';
+								conn.query(insertquery, function(err, insresult) {
+									if (err) throw err;
+									sess = req.session;
+									sess.username = user;
+									res.send('');
+								});
+							});
+						}
+						else {
+							res.send('<h4>Username/Email already taken</h4>');
+						}
+					});
+					conn.release();
+				});
+			}
+			else {
+				if (!validEmail && (pass != conpass))
+					res.send('<h4>Invalid email</h4><br /><h4>Passwords do not match</h4>');
+				else if (!validEmail)
+					res.send('<h4>Invalid email</h4>');
+				else
+					res.send('<h4>Passwords do not match</h4>');
+			}
+		}
 	});
 
 	app.get('/api/logout', function(req, res) {
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,9 +1,88 @@
 import React, { Component } from 'react';
+import { browserHistory } from 'react-router';
 
 class LoginForm extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {}
+		this.state = {
+			user: '',
+			email: '',
+			pass: '',
+			confirmpass: ''
+		}
+
+		this.handleUsername = this.handleUsername.bind(this);
+		this.handleEmail = this.handleEmail.bind(this);
+		this.handlePassword = this.handlePassword.bind(this);
+		this.handleConfirmPass = this.handleConfirmPass.bind(this);
+
+		this.loginSubmit = this.loginSubmit.bind(this);
+		this.registerSubmit = this.registerSubmit.bind(this);
+	}
+
+	handleUsername(e) {
+		this.setState({ user: e.target.value });
+	}
+
+	handleEmail(e) {
+		this.setState({ email: e.target.value });
+	}
+
+	handlePassword(e) {
+		this.setState({ pass: e.target.value });
+	}
+	handleConfirmPass(e) {
+		this.setState({ confirmpass: e.target.value });
+	}
+
+	loginSubmit(e) {
+		e.preventDefault();
+
+		var data = {
+			user: this.state.user,
+			pass: this.state.pass
+		};
+
+		$.ajax({
+			type: 'POST',
+			url: '/api/login',
+			data: data
+		})
+		.done((data) => {
+				if (data == '')
+					browserHistory.push('/');
+				else
+					$('#fail-message').html(data);
+		})
+		.fail((jqXhr) => {
+			console.log("AJAX failure");
+		})
+	}
+
+	registerSubmit(e) {
+		e.preventDefault();
+
+		var data = {
+			user: this.state.user,
+			email: this.state.email,
+			pass: this.state.pass,
+			confirmpass: this.state.confirmpass
+		};
+
+		$.ajax({
+			type: 'POST',
+			url: '/api/register',
+			data: data
+		})
+		.done((data) => {
+				if (data == '')
+					browserHistory.push('/');
+				else
+					$('#fail-message').html(data);
+		})
+		.fail((jqXhr) => {
+			console.log("AJAX failure");
+		})
 	}
 
 	render() {
@@ -11,46 +90,52 @@ class LoginForm extends Component {
 
 		if (this.props.value == "login") {
 			form = (
-				<form id="login-form" action="/api/login" method="post" role="form">
-					<div className="form-group">
-						<input type="text" name="username" id="username" tabIndex="1" className="form-control" placeholder="Username" />
-					</div>
-					<div className="form-group">
-						<input type="password" name="password" id="password" tabIndex="2" className="form-control" placeholder="Password" />
-					</div>
-					<div className="form-group">
-						<div className="row">
-							<div className="col-sm-6 col-sm-offset-3">
-								<input type="submit" name="login-submit" id="login-submit" tabIndex="3" className="form-control btn btn-success btn-logreg" value="Log In" />
+				<div>
+					<div id="fail-message" className="text-center text-danger"></div>
+					<form id="login-form" role="form" onSubmit={this.loginSubmit}>
+						<div className="form-group">
+							<input type="text" name="username" id="username" tabIndex="1" className="form-control" placeholder="Username" onChange={this.handleUsername} />
+						</div>
+						<div className="form-group">
+							<input type="password" name="password" id="password" tabIndex="2" className="form-control" placeholder="Password" onChange={this.handlePassword} />
+						</div>
+						<div className="form-group">
+							<div className="row">
+								<div className="col-sm-6 col-sm-offset-3">
+									<input type="submit" name="login-submit" id="login-submit" tabIndex="3" className="form-control btn btn-success btn-logreg" value="Log In" />
+								</div>
 							</div>
 						</div>
-					</div>
-				</form>
+					</form>
+				</div>
 			);
 		}
 		else {
 			form = (
-				<form id="register-form" action="/api/register" method="post" role="form">
-					<div className="form-group">
-						<input type="text" name="username" id="username" tabIndex="1" className="form-control" placeholder="Username" />
-					</div>
-					<div className="form-group">
-						<input type="email" name="email" id="email" tabIndex="1" className="form-control" placeholder="Email Address" />
-					</div>
-					<div className="form-group">
-						<input type="password" name="password" id="password" tabIndex="2" className="form-control" placeholder="Password" />
-					</div>
-					<div className="form-group">
-						<input type="password" name="confirmpassword" id="confirm-password" tabIndex="2" className="form-control" placeholder="Confirm Password" />
-					</div>
-					<div className="form-group">
-						<div className="row">
-							<div className="col-sm-6 col-sm-offset-3">
-								<input type="submit" name="register-submit" id="register-submit" tabIndex="3" className="form-control btn btn-success btn-logreg" value="Register" />
+				<div>
+					<div id="fail-message" className="text-center text-danger"></div>
+					<form id="register-form" role="form" onSubmit={this.registerSubmit}>
+						<div className="form-group">
+							<input type="text" name="username" id="username" tabIndex="1" className="form-control" placeholder="Username" onChange={this.handleUsername} />
+						</div>
+						<div className="form-group">
+							<input type="email" name="email" id="email" tabIndex="1" className="form-control" placeholder="Email Address" onChange={this.handleEmail} />
+						</div>
+						<div className="form-group">
+							<input type="password" name="password" id="password" tabIndex="2" className="form-control" placeholder="Password" onChange={this.handlePassword} />
+						</div>
+						<div className="form-group">
+							<input type="password" name="confirmpassword" id="confirm-password" tabIndex="2" className="form-control" placeholder="Confirm Password" onChange={this.handleConfirmPass} />
+						</div>
+						<div className="form-group">
+							<div className="row">
+								<div className="col-sm-6 col-sm-offset-3">
+									<input type="submit" name="register-submit" id="register-submit" tabIndex="3" className="form-control btn btn-success btn-logreg" value="Register" />
+								</div>
 							</div>
 						</div>
-					</div>
-				</form>
+					</form>
+				</div>
 			);
 		}
 
