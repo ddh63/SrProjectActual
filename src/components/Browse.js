cd8169a05c3d43164a176b9401df2557b6a9f139
ddh63 <ddh63@zips.uakron.edu>
2017-03-19 11:33:02 -0400

    new component and route, Browse

---
+++ src/components/Browse.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+import Nav from './Nav';
+
+class Browse extends Component {
+
+}
+
+export default Browse;
--- src/components/Routes.js
+++ src/components/Routes.js
@@ -3,6 +3,7 @@ import { Router, Route, browserHistory } from 'react-router';
 
 import Home from './Home';
 import Login from './Login';
+import Browse from './Browse';
 import NotFound from './NotFound';
 
 const Routes = (
@@ -10,6 +11,7 @@ const Routes = (
 		<Route path="/" component={Home} />
 		<Route path="login" component={Login} />
 		<Route path="register" component={Login} />
+		<Route path="browse" component={Browse} />
 		<Route path="*" component={NotFound} />
 	</Router>
 );
