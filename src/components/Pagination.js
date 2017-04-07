ded6de5274d5cc355ce558d14643c4387dff96e1
ddh63 <ddh63@zips.uakron.edu>
2017-04-06 21:46:24 -0400

    added logic for having ellipsis and last page show up on pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -4,8 +4,8 @@ class Pagination extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			current: 1,
-			count: 7
+			current: 28,
+			count: 35
 		}
 
 		this.pageChange = this.pageChange.bind(this);
@@ -27,16 +27,23 @@ class Pagination extends Component {
 
 		if (evenPageDisplay)
 			// Puts one less on left than right
-			pagesEitherSide = Math.floor(pagesToDisplay / 2) - 1;
+			pagesEitherSide = pagesToDisplay / 2 - 1;
 		else
 			pagesEitherSide = Math.floor(pagesToDisplay / 2);
 
 		// Logic to put more pages on left side when close to last page
 		if (this.state.count - this.state.current <= pagesEitherSide) {
 			pagesEitherSide += pagesEitherSide - (this.state.count - this.state.current);
-			if (evenPageDisplay) pagesEitherSide++;
+			if (evenPageDisplay) {
+				pagesEitherSide++;
+			}
 		}
 
+		if (this.state.current - pagesEitherSide > 1) {
+
+		}
+
+		let addToBack = true;
 		for (var i = this.state.current - pagesEitherSide; pagesAdded < pagesToDisplay; i++) {
 				if (i > this.state.count) 
 					break;
@@ -45,15 +52,48 @@ class Pagination extends Component {
 				if (i != this.state.current)
 					pages.push(<li key={i}><a href='#' onClick={this.pageChange}>{i}</a></li>);
 				else
-					pages.push(<li key={i}><a className='page-active' href='#' onClick={this.pageChange}>{i}</a></li>);
+					pages.push(<li key={i}><span className='page-active'>{i}</span></li>);
 				pagesAdded++;
+				if (pagesAdded == pagesToDisplay && i == this.state.count)
+					addToBack = false;
 		}
+
+		// Stupid amount of logic on whether or not to use ellipsis on end of pagination
+		let ellipsisBack = true;
+		let overrideEllipsis = false;
+
+		if (evenPageDisplay) {
+			if (this.state.current + pagesEitherSide == this.state.count - 2 || pagesToDisplay / 2 + this.state.current >= this.state.count)
+				ellipsisBack = false;
+			// Replaces ellipsis with second to last page
+			if (this.state.current + pagesEitherSide + 1 == this.state.count - 2)
+				overrideEllipsis = true;
+		}
+		else {
+			if (this.state.current + pagesEitherSide >= this.state.count - 1)
+				ellipsisBack = false;
+			// Replaces ellipsis with second to last page
+			if (this.state.current + pagesEitherSide == this.state.count - 2)
+				overrideEllipsis = true;
+		}
+
+
+		if (ellipsisBack) {
+			if (overrideEllipsis)
+				pages.push(<li key={this.state.count - 1}><a href="#" onClick={this.pageChange}>{this.state.count - 1}</a></li>);
+			else
+				pages.push(<li key={this.state.count - 1}><span className="ellipsis">...</span></li>);
+		}
+
+		if (addToBack)
+				pages.push(<li key={this.state.count}><a href="#" onClick={this.pageChange}>{this.state.count}</a></li>);
 
 		return (
 			<div className="container">
 				<div className="row">
 					<div className="pagination-container">
 						<ul className="pagination site">
+							<li><span>...</span></li>
 							{pages}
 						</ul>
 					</div>
