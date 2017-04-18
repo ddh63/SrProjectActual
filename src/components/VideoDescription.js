a1f5dc51e3732f3edde1c0095e63199f0488a454
ddh63 <ddh63@zips.uakron.edu>
2017-04-18 10:25:23 -0400

    styled the video page

--- src/components/VideoDescription.js
+++ src/components/VideoDescription.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import PageLink from './PageLink';
+
 const VideoDescription = (props) => {
 	let numGenres = props.video.genres.length;
 	let count = 0;
@@ -20,11 +22,9 @@ const VideoDescription = (props) => {
 	return (
 		<div>
 			<div className="container well description-title">
-				<h1>{props.video.title}</h1>
-				<p>{props.video.year}</p>
-			</div>
-			<div className="container well description">
-				<span>Genres: {genres}</span>
+				<h1 className="text-center site"><PageLink to={"/movie/"+props.video.id}>{props.video.title} ({props.video.year})</PageLink></h1>
+				<p className="text-center site">{genres}</p>
+				<p className="site video-plot">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 			</div>
 		</div>
 	);
